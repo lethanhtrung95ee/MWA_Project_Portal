b110694406e6cb7382d8165cf916a3f6d04f039f
lethanhtrung95ee <73425082+lethanhtrung95ee@users.noreply.github.com>
2022-10-25 00:27:40 -0500

    Deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/portal'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/portal/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
